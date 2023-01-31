6aee4ef3d50d5a26f942108725645c8ea19ef714
Saleh BUD <saleh.7734@gmail.com>
2023-01-31 13:00:21 +0200

    Components are separated

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
+import { useState } from 'react';
 import './App.scss';
+import { Board } from './Board';
 
-function App() {
-  return <div className="App"></div>;
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <button onClick={() => jumpTo(0)}>Go to game start</button>
+      </div>
+    </div>
+  );
 }
-
-export default App;
